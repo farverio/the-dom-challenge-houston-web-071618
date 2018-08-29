939e5e1eb7373751c5834658822b7ddf5b3b84d9
github username <github email address>
2018-08-29 15:38:42 -0500

    Done.

--- challenge.js
+++ challenge.js
@@ -1,50 +1,41 @@
-// As a user, i can like an individual number of the counter. I should see the appropriate number of likes associated with that particular number
-// As a user I can leave comments on my gameplay, such as "Wow, what a fun game this is"
-
 const counter = document.getElementById('counter');
 const minusButton = document.getElementById('-');
 const plusButton = document.getElementById('+');
+const minus5Button = document.getElementById('-5');
+const plus5Button = document.getElementById('+5');
 const likeButton = document.getElementById('<3');
 const likesText = document.querySelector('.likes');
 const pauseButton = document.getElementById('pause');
 const commentSubmitButton = document.getElementById('submit');
-let likeHash = {}
-let seconds = 0
+const form = document.getElementById('comment-form')
+let likeHash = {};
+let seconds = 0;
 let isPaused = false;
 
 setInterval(() => {
   if (!isPaused){
-    displayLikes()
-    changeCounter('+')
+    changeCounter(1);
   } 
 }, 1000)
 
-function changeCounter(action) {
-  if (action === '-') {
-    seconds--;
-  } else if (action === '+') {
-    seconds++;
-  }
+function changeCounter(num) {
+  seconds += num
   displayLikes()
   counter.innerHTML = seconds;
 }
 
 function displayLikes() {
   likeHash[seconds] = (likeHash[seconds] || 0);
-  likesText.innerHTML = `Total Likes: ${likeHash[seconds]}`
+  likesText.innerHTML = `Total Likes: ${likeHash[seconds]}`;
 }
 
 function addLike() {
   likeHash[seconds] = likeHash[seconds] + 1;
-  displayLikes()
+  displayLikes();
 }
 
 function togglePause() {
-  if (isPaused) {
-    pauseButton.innerHTML = "pause"
-  } else {
-    pauseButton.innerHTML = "resume"
-  }
+  isPaused ? pauseButton.innerHTML = "pause" : pauseButton.innerHTML = "resume";
   isPaused = !isPaused;
 }
 
@@ -55,12 +46,16 @@ function addComment(e) {
   li.appendChild(document.createTextNode(commentText));
   ul.appendChild(li);
   e.preventDefault;
+  form.reset();
 }
 
-minusButton.addEventListener('click', () => changeCounter('-'));
-plusButton.addEventListener('click', () => changeCounter('+'));
-likeButton.addEventListener('click', () => addLike());
+minusButton.addEventListener('click', ()=> changeCounter(-1));
+plusButton.addEventListener('click', ()=> changeCounter(1));
+minus5Button.addEventListener('click', ()=> changeCounter(-5));
+plus5Button.addEventListener('click', ()=> changeCounter(5));
+likeButton.addEventListener('click', ()=> addLike());
 pauseButton.addEventListener('click', ()=> togglePause());
+form.addEventListener('submit', e => e.preventDefault())
 commentSubmitButton.addEventListener('click', (e)=> addComment(e));
 
 
